bd87ec6ca98e81473eba1d9268111e84f0c868e2
Shashwat Nayak <shashwatnayak@outlook.com>
2019-01-03 23:24:50 +0530

    vgg initial req files added

--- Basics/CNN/myimponk/vgg.py
+++ Basics/CNN/myimponk/vgg.py
@@ -1,1 +1,10 @@
 import keras
+import numpy as np
+from keras.model import Sequential # for sequential cnn
+from keras.layers import Dense,Dropout,Flatten # reg - dropout, flatten for FC
+from keras.layers import Conv2D, MaxPooling2D # Inside NN config uses conv 2d and maxpool2d
+from keras.optimizers import SGD # sgd used. will consider adam
+
+
+
+
