4e68d0c795e551dffe2159a89e045f062cc2df3e
Shashwat Nayak <shashwatnayak@outlook.com>
2019-01-03 23:30:56 +0530

    to be done again

--- Basics/CNN/myimponk/vgg.py
+++ Basics/CNN/myimponk/vgg.py
@@ -1,6 +1,6 @@
 import keras
 import numpy as np
-from keras.model import Sequential # for sequential cnn
+from keras.models import Sequential # for sequential cnn
 from keras.layers import Dense,Dropout,Flatten # reg - dropout, flatten for FC
 from keras.layers import Conv2D, MaxPooling2D # Inside NN config uses conv 2d and maxpool2d
 from keras.optimizers import SGD # sgd used. will consider adam
@@ -36,3 +36,31 @@ model.compile(loss='categorical_crossentropy', optimizer=sgd)
 
 model.fit(x_train, y_train, batch_size=32, epochs=10)
 score = model.evaluate(x_test, y_test, batch_size=32)
+
+
+# Using TensorFlow backend.
+# Epoch 1/10
+# 2019-01-03 23:28:39.917557: I tensorflow/core/platform/cpu_feature_guard.cc:137] Your CPU supports instructions that this TensorFlow binary was not compiled to use: SSE4.1 SSE4.2 AVX AVX2 FMA
+# 100/100 [==============================] - 6s 64ms/step - loss: 2.3081
+# Epoch 2/10
+# 100/100 [==============================] - 6s 57ms/step - loss: 2.3382
+# Epoch 3/10
+# 100/100 [==============================] - 6s 58ms/step - loss: 2.2872
+# Epoch 4/10
+# 100/100 [==============================] - 6s 55ms/step - loss: 2.3054
+# Epoch 5/10
+# 100/100 [==============================] - 6s 56ms/step - loss: 2.2914
+# Epoch 6/10
+# 100/100 [==============================] - 6s 55ms/step - loss: 2.2853
+# Epoch 7/10
+# 100/100 [==============================] - 6s 55ms/step - loss: 2.2765
+# Epoch 8/10
+# 100/100 [==============================] - 6s 55ms/step - loss: 2.2723
+# Epoch 9/10
+# 100/100 [==============================] - 6s 55ms/step - loss: 2.2858
+# Epoch 10/10
+# 100/100 [==============================] - 6s 55ms/step - loss: 2.2847
+# 20/20 [==============================] - 0s 21ms/step
+
+#Conclusion not that good.
+#Will look towards improving this net.
