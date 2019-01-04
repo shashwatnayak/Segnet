cdd3481b6b1d46e9ab5f70a9b11b40a7292b3183
Shashwat Nayak <shashwatnayak@outlook.com>
2019-01-04 20:39:32 +0530

    completed and not run

--- Basics/CNN/myimponk/alexnet.py
+++ Basics/CNN/myimponk/alexnet.py
@@ -24,4 +24,38 @@ model.add(BatchNormalization())
 model.add(Activation='relu')
 model.add(MaxPooling2D(pool_size=(2,2)))
 
+model.add(ZeroPadding((1,1)))
+model.add(Conv2D(512,(3,3)padding = 'same'))
+model.add(BatchNormalization())
+model.add(Activation='relu')
+model.add(MaxPooling2D(pool_size = (2,2)))
+
+model.add(ZeroPadding((1,1)))
+model.add(Conv2D(512,(3,3),padding = 'same'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2,2)))
+
+model.add(ZeroPadding((1,1)))
+model.add(Conv2D(1024,(3,3),padding = 'same'))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size = (2,2)))
+
+model.add(Flatten())
+model.add(Dense(3072))
+model.add(BatchNormalization())
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+
+model.add(Dense(4096))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+
+model.add(Dense(10))
+model.add(BatchNormalization())
+model.add(Activation('softmax'))
+
 
