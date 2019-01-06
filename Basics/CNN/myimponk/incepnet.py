a555338820d461e1eae44efef6acaceaf894bb3d
Shashwat Nayak <shashwatnayak@outlook.com>
2019-01-06 11:30:44 +0530

    few more lines added

--- Basics/CNN/myimponk/incepnet.py
+++ Basics/CNN/myimponk/incepnet.py
@@ -1,4 +1,5 @@
 import keras
+import os
 from keras.datasets import cifar10
 from keras.utils import np_utils
 from keras.layers import Input
@@ -37,5 +38,22 @@ out = Dense(10,activation = 'softmax')(output)
 
 
 model = Model(inputs = input_img,output = out)
-print model.summary()
+print (model.summary())
+
+epochs = 25
+lrate = 0.01
+decay = lrate/epochs
+sgd = SGD(lr = lrate,momentum = 0.9,decay = decay,nesterov = False)
+
+model.compile(loss = 'categorical_crossentropy',optimizer = sgd,metrics = ['accuracy'])
+
+model.fit(X_train,y_train,validation_data = (X_test,y_test),epochs = epochs,batch_size = 32)
+
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+model.save_weights(os.path.join(os.getcwd(), 'model.h5'))
+
+scores = model.evaluate(X_test, y_test, verbose=0)
+print("Accuracy: %.2f%%" % (scores[1]*100))
 
